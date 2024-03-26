76ada8db267ae83343a09685f7e29e496a9bef2c
KB-231_Kasilov <vyacheslav.kasilov631842@mail.ru>
2024-03-26 16:27:45 +0300

    refactoring

--- main.c
+++ main.c
@@ -31,33 +31,34 @@ void test_popBack_notEmptyVector() {
     assert(v.size == 0);
     assert(v.capacity == 1);
 }
-void test_atVector_notEmptyVector(){
-    vector  v = createVector(5);
-    for(int i = 1; i < v.capacity; i++){
+
+void test_atVector_notEmptyVector() {
+    vector v = createVector(5);
+    for (int i = 1; i < v.capacity; i++) {
         pushBack(&v, i);
     }
     int *index = atVector(&v, 2);
     assert(*index == 3);
 }
 
-void test_atVector_requestToLastElement(){
-    vector  v = createVector(5);
-    for(int i = 0; i < v.capacity; i++){
+void test_atVector_requestToLastElement() {
+    vector v = createVector(5);
+    for (int i = 0; i < v.capacity; i++) {
         pushBack(&v, i);
     }
     int *index = atVector(&v, 4);
     assert(*index == 4);
 }
 
-void test_back_oneElementInVector(){
+void test_back_oneElementInVector() {
     vector v = createVector(0);
     pushBack(&v, 5);
     int *index = atVector(&v, 0);
     assert(*index == 5);
 }
 
-void test_front_oneElementInVector(){
-    vector  v = createVector(1);
+void test_front_oneElementInVector() {
+    vector v = createVector(1);
     pushBack(&v, 5);
     int *pFront = front(&v);
     assert(*pFront == 5);
